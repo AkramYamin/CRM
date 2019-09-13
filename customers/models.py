bdde0fc4b5e299e1f5a33d1fcacb4651ca4e88c1
Akram Yamin <ak.5@yahoo.com>
2019-09-13 10:55:22 +0300

    change database design and use django.contrib.auth.models

--- customers/models.py
+++ customers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -16,20 +17,10 @@ class Service(models.Model):
 
 
 class Employee(models.Model):
-    name = models.CharField(max_length=250)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
     phone = PhoneNumberField()
-    username = models.CharField(max_length=15)
-    password = models.CharField(max_length=500)
     first_address = models.CharField(max_length=500)
     second_address = models.CharField(max_length=500)
-    email = models.EmailField()
-    is_admin = models.BooleanField(default=True)
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
 
 
 class Customer(models.Model):
